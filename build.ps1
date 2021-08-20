9573ec18d2593097e33903e9babb5166c71f072f
Scott Williams <tocsoft@gmail.com>
2021-08-20 15:36:43 +0100

    target framework

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-dotnet publish -r win10-x64 -o publish/win -c Release
-# dotnet publish -r linux-x64 -o publish/linux -c Release
+dotnet publish ./KubeConnect/KubeConnect.csproj -r win10-x64 -o publish/win -c Release
+# dotnet publish ./KubeConnect/KubeConnect.csproj -r linux-x64 -o publish/linux -c Release
