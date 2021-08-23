4c1eeaabf38ff09b9d4ac5ebbec6e8ab74ada307
Scott Williams <tocsoft@gmail.com>
2021-08-23 17:14:19 +0100

    Create dotnet tool package and build framework dependent exe

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-dotnet publish ./KubeConnect/KubeConnect.csproj -r win10-x64 -o publish/win -c Release
-# dotnet publish ./KubeConnect/KubeConnect.csproj -r linux-x64 -o publish/linux -c Release
+dotnet publish ./KubeConnect/KubeConnect.csproj -r win10-x64 -o publish/win -c Release -p:PublishSingleFile=true
+dotnet pack ./KubeConnect/KubeConnect.csproj -o publish/tool -c Release
