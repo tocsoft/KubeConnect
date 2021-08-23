44ef373fbfe619892c547ea6750e03af150e1527
Scott Williams <tocsoft@gmail.com>
2021-08-23 21:25:52 +0100

    self contained flag

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-dotnet publish ./KubeConnect/KubeConnect.csproj --runtime win10-x64 -o publish/win -c Release --framework net5.0
+dotnet publish ./KubeConnect/KubeConnect.csproj --runtime win10-x64 -o publish/win -c Release --framework net5.0  --self-contained
 dotnet pack ./KubeConnect/KubeConnect.csproj -o publish/tool -c Release
