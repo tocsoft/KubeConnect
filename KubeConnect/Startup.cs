3b05c991b567799951c8219d1aec14e2a91927d4
Scott Williams <tocsoft@gmail.com>
2022-05-17 16:01:53 +0100

    cleanup middleware

--- KubeConnect/Startup.cs
+++ KubeConnect/Startup.cs
@@ -22,27 +22,14 @@ namespace KubeConnect
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
             services.AddReverseProxy();
             services.AddSingleton<IProxyConfigProvider, IngressProxyConfig>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
